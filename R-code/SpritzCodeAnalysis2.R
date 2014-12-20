03b82fae30b1bb93a28906099e897001f18e4629
MAydogdu <murt001@gmail.com>
2014-12-20 13:23:38 -0500

    Revised analysis

--- R-code/SpritzCodeAnalysis2.R
+++ R-code/SpritzCodeAnalysis2.R
@@ -5,42 +5,62 @@ options(digits=4)
 library("pastecs", lib.loc="~/R/win-library/2.15")
 library("sqldf", lib.loc="/Library/Frameworks/R.framework/Versions/2.15/Resources/library")
 table(SpritzMain2$FS)
+table(SpritzMain2$Age)
+
+
+# For univariate descriptive statistics useSpritzMain: one observation per subject
+#Age distribution
+c  <-ggplot(SpritzMain, aes(Age)) + geom_bar(binwidth=5) + coord_flip()+
+     labs( title = "Age distribution of the subjects", x="Age", y="Count")+
+     theme(panel.grid.major.x  = element_line(colour = "grey95", size=0.25, linetype = "solid"), 
+        panel.grid.major.y  = element_line(colour = "grey95", size=0.25, linetype = "solid"), 
+        axis.ticks = element_blank(),
+        axis.text.x = element_text(size=8), 
+        panel.background = element_blank(),
+        panel.border = element_blank())
+c
+
+#Gender
+c  <-ggplot(SpritzMain, aes(Female)) + geom_bar(binwidth=1) + #coord_flip()+
+  labs( title = "Gender distribution", x="Gender (Female=1)", y="Count")+
+  theme(panel.grid.major.x  = element_line(colour = "grey95", size=0.25, linetype = "solid"), 
+        panel.grid.major.y  = element_line(colour = "grey95", size=0.25, linetype = "solid"), 
+        axis.ticks = element_blank(),
+        axis.text.x = element_text(size=8), 
+        panel.background = element_blank(),
+        panel.border = element_blank())+
+  scale_x_discrete(name  ="",
+                       breaks=c(0,1),
+                       labels=c("Male", "Female")) 
+c
+
+table(SpritzMain$PrimEng)
+table(SpritzMain$Female)
+table(SpritzMain$RaceCode)
+c  <-ggplot(SpritzMain, aes(DegreeCode)) + geom_bar(binwidth=1) + #coord_flip()+
+  labs( title = "Highest degree or level of education", x="Gender (Female=1)", y="Count")+
+  theme(panel.grid.major.x  = element_line(colour = "grey95", size=0.25, linetype = "solid"), 
+        panel.grid.major.y  = element_line(colour = "grey95", size=0.25, linetype = "solid"), 
+        axis.ticks = element_blank(),
+        axis.text.x = element_text(size=8), 
+        panel.background = element_blank(),
+        panel.border = element_blank())+
+  scale_x_discrete(name  ="",
+                   breaks=c(1,2,3,4,5,6),
+                   labels=c("","HS","","","Bach.","Grad.")) 
+c
+
+table(SpritzMain$readTM)
+table(SpritzMain$readBook)
+table(SpritzMain$readSci)
+table(SpritzMain$readMag)
+table(SpritzMain$readProf)
+table(SpritzMain$SpritzExp)
 
 # For each subject, so use half of the observations
 sqldf("select firstNormal, firstFun, count(*) from SpritzMain2 where FS='FUN' group by firstNormal, firstFun")
 sqldf("select firstFun, count(*) from SpritzMain2 where FS='FUN' 
               and firstNormal = 1 ")
-
-library("ggplot2", lib.loc="/Library/Frameworks/R.framework/Versions/2.15/Resources/library")
-g <-ggplot(SpritzMain2, aes(FS, Y, colour = factor(trt))) + geom_point(position = position_jitter(w = 0.2, h = 0.2))+
-  labs( title = "Number of correct answers by article type and treatment (trt=1 is Spritz)", x="Article", y="Correct answers")+
-  theme(panel.grid.major.x  = element_line(colour = "grey95", size=0.25, linetype = "solid"), 
-        panel.grid.major.y  = element_line(colour = "grey95", size=0.25, linetype = "solid"), 
-        axis.ticks = element_blank(),
-        axis.text.x = element_text(size=8), 
-        panel.background = element_blank(),
-        panel.border = element_blank(),
-        legend.title = element_text(colour="blue", size=16, face="bold"))+
-  scale_shape_discrete(name  ="Type",
-                       breaks=c(0,1),
-                       labels=c("Normal", "Spritz"))  
-g
-
-g <-ggplot(SpritzMain2, aes(FS, Y, colour = factor(trt))) + geom_boxplot()+
-  labs( title = "Number of correct answers by article type and treatment (trt=1 is Spritz)", x="Article", y="Correct answers")+
-  theme(panel.grid.major.x  = element_line(colour = "grey95", size=0.25, linetype = "solid"), 
-        panel.grid.major.y  = element_line(colour = "grey95", size=0.25, linetype = "solid"), 
-        axis.ticks = element_blank(),
-        axis.text.x = element_text(size=8), 
-        panel.background = element_blank(),
-        panel.border = element_blank(),
-        legend.title = element_text(colour="blue", size=16, face="bold"))+
-  scale_shape_discrete(name  ="Type",
-                       breaks=c(0,1),
-                       labels=c("Normal", "Spritz"))  
-
-g
-
 
 #Univariate statistics
 stat.desc(SpritzMain2$Y)
@@ -68,7 +88,8 @@ ols(Y ~ trt*isSAD,SpritzMain2,cluster="Meta_code")
 
 summary(lm(SpritzMain2$Y ~ SpritzMain2$trt * SpritzMain2$isSAD + SpritzMain2$firstNormal))
 
-summary(lm(SpritzMain2$Y ~ SpritzMain2$trt + SpritzMain2$isSAD+ SpritzMain2$firstNormal 
+summary(lm(SpritzMain2$Y ~ SpritzMain2$trt + SpritzMain2$isSAD + SpritzMain2$firstNormal 
+           + SpritzMain2$Age 
            + SpritzMain2$SpritzExp + SpritzMain2$UsesGlasses + SpritzMain2$DegreeCode
            + SpritzMain2$PrimEng + SpritzMain2$Female + SpritzMain2$RaceCode 
            + SpritzMain2$readTM + SpritzMain2$readBook + SpritzMain2$readSci 
@@ -112,3 +133,25 @@ ols <- function(form, data, robust=FALSE, cluster=NULL,digits=3){
 }
 
 
+
+summary(SpritzMain2$Y[SpritzMain2$FS == "FUN" & SpritzMain2$trt == 1]) 
+summary(SpritzMain2$Y[SpritzMain2$FS == "FUN" & SpritzMain2$trt == 0]) 
+summary(SpritzMain2$Y[SpritzMain2$FS == "SAD" & SpritzMain2$trt == 1]) 
+summary(SpritzMain2$Y[SpritzMain2$FS == "SAD" & SpritzMain2$trt == 0]) 
+means <- aggregate(SpritzMain2$Y, by=list(SpritzMain2$FS, SpritzMain2$trt), FUN=mean)
+table(SpritzMain2$FSTRT)
+SpritzMain2$FSTRT <- ifelse(SpritzMain2$trt == 1, paste(SpritzMain2$FS, "SPR"), paste(SpritzMain2$FS, "NOR"))
+ggplot(data=SpritzMain2, aes(x=FSTRT, y=Y, fill=FSTRT)) + 
+  geom_boxplot() +
+  stat_summary(fun.y=mean, colour="darkred", geom="point", 
+               shape=18, size=4, show_guide = FALSE) + 
+  stat_summary(fun.y=mean, colour="red", geom="text", show_guide = FALSE, vjust=-1, aes(label=round(..y.., digits=1)))+
+  labs( title = "A summary of the experiment results", x="Article and type (and mean correct answer)", y="Correct answers")+
+  theme(panel.grid.major.x  = element_line(colour = "grey95", size=0.25, linetype = "solid"), 
+        panel.grid.major.y  = element_line(colour = "grey95", size=0.25, linetype = "solid"), 
+        axis.ticks = element_blank(),
+        axis.text.x = element_text(size=8), 
+        panel.background = element_blank(),
+        panel.border = element_blank(),
+        legend.position = "none")+
+  scale_fill_manual(values=(c('gold', 'gold3','cyan','cyan3')))
